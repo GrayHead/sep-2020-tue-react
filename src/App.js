09de3f6cbdc32c70f9827f3f0d3d8564ad5b331a
GrayHead <ss.zhuravlov@gmail.com>
2020-12-03 20:05:55 +0200

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import {users} from './database/usersDB';
-import UserComponent from './components/user/UserComponent';
 
 
 class App extends Component {
@@ -9,17 +7,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				{
-					users.map((user, index) => {
-						let clsX = index % 2 ? 'even' : 'odd';
 
-						return (
-							<UserComponent
-								item={user}
-								cls={clsX}
-								key={index}/>);
-					})
-				}
 			</div>
 		);
 	}
